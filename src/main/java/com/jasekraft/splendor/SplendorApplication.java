55ccfffa30ffded9994c44a9cb973193d8c9c233
Jase Kraft <jase@perfectmypodcast.com>
2021-10-14 15:40:41 -0500

    initial commit

---
+++ src/main/java/com/jasekraft/splendor/SplendorApplication.java
@@ -0,0 +1,13 @@
+package com.jasekraft.splendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasekraft/splendor/SplendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasekraft.splendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
