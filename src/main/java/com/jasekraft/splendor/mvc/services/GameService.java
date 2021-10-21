579c9cd80f93c5a90e16f5b9ac1d5fcf3ac87ffb
Lyrenyx Maxima <haishenrong@gmail.com>
2021-10-21 15:05:28 -0400

    Minor fixes to files

--- src/main/java/com/jasekraft/splendor/mvc/services/GameService.java
+++ src/main/java/com/jasekraft/splendor/mvc/services/GameService.java
@@ -48,7 +48,7 @@ public class GameService {
 		nobleServ.init(game, totalNobles);
 		//Add tokens to the game
 		tokenServ.init(game, totalTokens);
-		game.setTurn(1);
+		//game.setTurn(1);
 		update(game);
 		//gameRepo.save(game);
 	}
